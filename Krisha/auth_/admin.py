a966d9c1e68a1bccc91e3bf4dec8183f105ddef4
ulan-sharinbekov <73186262+ulan-sharinbekov@users.noreply.github.com>
2022-06-17 17:19:04 +0600

    Create admin.py

---
+++ Krisha/auth_/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import MyUser, Profile
+
+
+@admin.register(MyUser)
+class MyUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'role', 'birthday')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'card_number', 'city')
