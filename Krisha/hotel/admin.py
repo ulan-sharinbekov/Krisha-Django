7bda2b4c79acfe51393638a51243e2c1210de8c2
ulan-sharinbekov <73186262+ulan-sharinbekov@users.noreply.github.com>
2022-06-20 10:20:29 +0600

    Create admin.py

---
+++ Krisha/hotel/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Hotel
+
+
+# Register your models here.
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'type_by_star', 'city')
+    search_fields = ['name', 'type_by_star', 'city']
