8026078f9a1f72b42cd6a4f8f8a2aadeca5c52aa
ulan-sharinbekov <73186262+ulan-sharinbekov@users.noreply.github.com>
2022-06-20 10:18:53 +0600

    Create urls.py

---
+++ Krisha/comment/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+router = DefaultRouter()
+
+router.register(r'', views.CommentViewSet, basename="comments")
+
+urlpatterns = router.
